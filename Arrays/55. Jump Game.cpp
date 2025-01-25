58b84e2ffe6876930675df852bf0f70c47610157
Nikhil Maurya <69952735+nikkhilmaurya@users.noreply.github.com>
2025-01-25 16:27:23 +0530

    Create 55. Jump Game.cpp

---
+++ Arrays/55. Jump Game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int m = 0;
+        for(int i = 0; i < n; i++){
+            if(i>m) return false;
+            m = max(m,i+nums[i]);
+            if(m>=n-1) return true;
+        }
+        return false;
+    }
+};
